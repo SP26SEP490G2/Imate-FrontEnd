394657b4ae7be612af3c72adb8c5d8ecbdf753e4
PhuDK <PhuDKHE186439@fpt.edu.vn>
2026-03-02 17:05:13 +0700

    git push
    tao dependency, dung "npm install"

--- imate_frontend/vite.config.ts
+++ imate_frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import * as fs from 'fs';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const projectRoot = path.resolve(__dirname, './');
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: parseInt(env.VITE_PORT) || 7939,
+      host: "localhost",
+      // https: true, 
+    },
+  };
+});
